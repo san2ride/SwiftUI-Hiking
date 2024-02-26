d2d9aac563b57750a8716f1fc7842c076340e931
Jason <sanchezjason@me.com>
2024-02-26 15:29:43 -0700

    swiftui/hiking- Added images

---
+++ Hiking/Hiking/HikingApp.swift
@@ -0,0 +1,17 @@
+//
+//  HikingApp.swift
+//  Hiking
+//
+//  Created by Cause  I'm Electric on 2/26/24.
+//
+
+import SwiftUI
+
+@main
+struct HikingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
